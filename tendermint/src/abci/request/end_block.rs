6c703e5bfca66836079e561956f94441d9d9cc85
Henry de Valence <hdevalence@hdevalence.ca>
2021-04-13 15:35:51 -0700

    Add ABCI domain types.
    These types mirror the generated types in tendermint_proto, but have better
    naming.  The documentation is filled in from the ABCI methods & types
    documentation.

---
+++ tendermint/src/abci/request/end_block.rs
@@ -0,0 +1,41 @@
+/// Signals the end of a block.
+///
+/// Called after all transactions, and prior to each `Commit`.
+///
+/// [ABCI documentation](https://docs.tendermint.com/master/spec/abci/abci.html#endblock)
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct EndBlock {
+    /// The height of the block just executed.
+    pub height: i64,
+}
+
+// =============================================================================
+// Protobuf conversions
+// =============================================================================
+
+// XXX(hdevalence): these all use &'static str for now, this should be fixed
+// to align with the crate's error-handling strategy.
+
+use std::convert::TryFrom;
+use tendermint_proto::abci as pb;
+use tendermint_proto::Protobuf;
+
+impl From<EndBlock> for pb::RequestEndBlock {
+    fn from(end_block: EndBlock) -> Self {
+        Self {
+            height: end_block.height,
+        }
+    }
+}
+
+impl TryFrom<pb::RequestEndBlock> for EndBlock {
+    type Error = crate::Error;
+
+    fn try_from(end_block: pb::RequestEndBlock) -> Result<Self, Self::Error> {
+        Ok(Self {
+            height: end_block.height,
+        })
+    }
+}
+
+impl Protobuf<pb::RequestEndBlock> for EndBlock {}
